d50c1927c650d327c783854e01cbf3d1adf7a0e6
Astha86 <asthasahani08@gmail.com>
2024-05-18 07:08:48 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Himanshi Singh",
+      job: "Software Engineer",
+      image:"https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/short/linkedin-profile-picture-maker/dummy_image/thumb/004.webp",
+      text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+    },
+    {
+      id: 2,
+      name: "Bhavya Wangdu",
+      job: "Graphic Designer",
+      image: "https://www.befunky.com/images/wp/wp-2021-01-linkedin-profile-picture-before.jpg?auto=avif,webp&format=jpg&width=944",
+      text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+    },
+    {
+      id: 3,
+      name: "John Wang",
+      job: "Marketing Manager",
+      image:"https://i.pinimg.com/236x/07/33/ba/0733ba760b29378474dea0fdbcb97107.jpg",
+      text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+    },
+    {
+      id: 4,
+      name: "Olivia Williams",
+      job: "Content Strategist",
+      image:"https://images.squarespace-cdn.com/content/v1/5aee389b3c3a531e6245ae76/1531792846005-MYGZAOI0L93I3YJWHB6W/D75_5697-Edit.jpg",
+      text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+    },
+    {
+      id: 5,
+      name: "	Jarvis Katz",
+      job: "Data Analyst",
+      image: "https://www.davidchang.ca/wp-content/uploads/2020/09/David-Chang-Photography-Headshots-Toronto-61-1536x1536.jpg",
+      text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+    },
+  ];
+  
+  export default reviews;
